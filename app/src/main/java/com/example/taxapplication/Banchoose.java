121b78ba65f906c9add5cc4f60a71665d3ff1ecd
tamim1122 <60109107+tamim1122@users.noreply.github.com>
2020-03-01 22:14:50 +0600

    Add Language

---
+++ app/src/main/java/com/example/taxapplication/Banchoose.java
@@ -0,0 +1,14 @@
+package com.example.taxapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Banchoose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banchoose);
+    }
+}
